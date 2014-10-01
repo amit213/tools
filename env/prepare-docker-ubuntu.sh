a221a5787e0ca04ad6c6a870f14746cb13d59f65
Amit <mbuser@bigrock.local>
2014-10-01 01:57:57 -0700

    prepare-docker v0.1

---
+++ env/prepare-docker-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#!/bin/bash
+#ss
+
+#### this is prep work for running docker on fresh Docker containers.
+
+#install docker.
+
+echo  "This script needs sudo access"
+
+sudo apt-get update
+sudo apt-get install apt-transport-https -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install lxc-docker -y
+
+echo "Docker installation done."
+echo "Adding $USER to docker group"
+sudo adduser $USER docker
+
