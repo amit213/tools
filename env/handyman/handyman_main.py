a312e0ecdb8677554c4943a166fc99ff906ccdb8
Amit <amit213@users.noreply.github.com>
2014-10-31 23:38:32 -0700

    handyman infra update

--- env/handyman/handyman_main.py
+++ env/handyman/handyman_main.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-
 #
 #wrapper tool for automation and env config.
 #
+from handyutil import *
+
+
 
 def printVer():
-	print "handyman version :", selfVer
-	return
+  print "handyman version :", selfVer
+  utilVer()
+  return
 
 
 if __name__ == "__main__":
-	selfVer = "0.0.1"
-	printVer()
+			selfVer = "0.0.1"
+			printVer()
 
---
+++ env/handyman/handyutil.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+#util used by wrapper tool for automation and env config.
+#
+
+def utilVer():
+        selfVer = "0.0.1u"
+	print "util version :", selfVer
+	return
+
