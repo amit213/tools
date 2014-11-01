205a80dbd43635733e22085dcc498bb1c85bc217
Amit <amit213@users.noreply.github.com>
2014-10-31 23:08:40 -0700

    porting python tool "handyman". initial version.

---
+++ env/handyman/handyman_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#
+#wrapper tool for automation and env config.
+#
+
+def printVer():
+	print "handyman version :", selfVer
+	return
+
+
+if __name__ == "__main__":
+	selfVer = "0.0.1"
+	printVer()
+
