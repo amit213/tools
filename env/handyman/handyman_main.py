0170e6fac72514207f737252943dd1c4cd0d7cfc
Amit <amit213@users.noreply.github.com>
2014-11-01 19:23:26 -0700

    updates. handyman tool infra.

--- env/handyman/handyman_main.py
+++ env/handyman/handyman_main.py
@@ -3,6 +3,49 @@
 #wrapper tool for automation and env config.
 #
 from handyutil import *
+import argparse
+
+
+class handyMantool(object):
+      def __init__(self):   
+       self.param_table = []
+       return
+      def init_tool_params(self):
+       self.param_table = [
+                            ['club', 'what does club param do'],
+                            ['clean', 'what does clean param do'],
+                            ['sample', 'what does sample param do'],
+                          ]
+       return
+      def getParamTable(self):
+       return self.param_table
+
+      @classmethod
+      def init(cls):       
+       return cls
+
+
+class sshBookmark:
+      #
+      # store, launch and run ssh sessions.
+      #
+      def __init__(self):
+       self.name="empty"
+       return
+
+      def __init__(self, name="null", 
+                   connectToPort="22",
+                   username="user"):
+       self.name=name
+       self.connectToPort=connectToPort
+       self.username=username
+       self.connectToIP=""
+       return
+
+
+      def list_bookmark(self):
+       print self.name
+       return
 
 
 
@@ -11,8 +54,24 @@ def printVer():
   utilVer()
   return
 
+def create_ssh_bookmark():
+  s1 = sshBookmark()
+  s1.list_bookmark()
+  return
 
-if __name__ == "__main__":
-			selfVer = "0.0.1"
-			printVer()
+def main():
+  global h1
+  #selfVer = "0.0.1"
+  #printVer()
+  #create_ssh_bookmark()
+  
+  h1 = handyMantool()  
+  h1.init_tool_params()  
+  utilParseParams(h1) 
+  return
+
+h1 = None
+
+if __name__ == "__main__":  main()
+
 
--- env/handyman/handyutil.py
+++ env/handyman/handyutil.py
@@ -2,9 +2,35 @@
 #
 #util used by wrapper tool for automation and env config.
 #
+import argparse
+
+#from handyman_main import handyMantool
+import handyman_main
+
+#from handyman_main import pSelf
+#import handyman_main as mm
+#from handyman_main import h1
+
+def myprint(args):
+  print args
+  return
 
 def utilVer():
-        selfVer = "0.0.1u"
+	selfVer = "0.0.1u"
 	print "util version :", selfVer
 	return
 
+
+def utilParseParams(tmpObj=None):
+	
+	parser1 = argparse.ArgumentParser()
+
+	parser1.add_argument('greet', nargs='?', help='issue a greeting')
+	parser1.add_argument('opentunnels', nargs='?', help='open tunnels based on envconfig')
+	parser1.add_argument('listopentunnels', help='open tunnels based on envconfig', nargs='?')
+	
+	for entries in tmpObj.getParamTable():		
+		parser1.add_argument(entries[0], help=entries[1], nargs='?')
+
+	parser1.parse_args()                    
+	return
