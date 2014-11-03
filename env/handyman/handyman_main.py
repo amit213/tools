c7cdd004de1dc4911f909880561e1a2f9f22c069
Amit <amit213@users.noreply.github.com>
2014-11-02 18:00:52 -0800

    event infra for tool. action functions prep.

--- env/handyman/handyman_main.py
+++ env/handyman/handyman_main.py
@@ -7,22 +7,63 @@ import argparse
 
 
 class handyMantool(object):
+      _toolObj = None
       def __init__(self):   
        self.param_table = []
+       self._tag = None
+       self._eventList = []       
        return
       def init_tool_params(self):
        self.param_table = [
                             ['club', 'what does club param do'],
                             ['clean', 'what does clean param do'],
                             ['sample', 'what does sample param do'],
-                          ]
+                          ]       
+       return
+      def appendParamTable(self, value=None):
+       self.param_table.append(value)
        return
       def getParamTable(self):
        return self.param_table
+      @property
+      def tag(self):
+          return self._tag
+      @tag.setter
+      def tag_setter(self, value):
+          self._tag = value          
+      
 
       @classmethod
       def init(cls):       
        return cls
+      @classmethod
+      def getToolInstance(self): 
+       return handyMantool._toolObj
+
+      @classmethod
+      def setToolInstance(self, toolObj):
+       handyman_main.handyMantool._toolObj = toolObj
+       return
+
+      def holaecho(self, hola='Florida'):
+       print "echoing :", hola
+       pass
+
+      def enqueue_new_event(self, eventObj=None):
+       self._eventList.append(eventObj)
+       return
+
+      @property
+      def eventList(self):
+       return self._eventList
+
+      def process_all_events(self):
+       print "processing all events"
+       for evt in self.eventList:
+        print evt.event_name
+        pass
+       return 
+
 
 
 class sshBookmark:
@@ -60,17 +101,19 @@ def create_ssh_bookmark():
   return
 
 def main():
-  global h1
+
   #selfVer = "0.0.1"
   #printVer()
   #create_ssh_bookmark()
   
-  h1 = handyMantool()  
+  h1 = handyMantool()
+  h1.setToolInstance(h1)
   h1.init_tool_params()  
   utilParseParams(h1) 
+  h1.process_all_events()
   return
 
-h1 = None
+
 
 if __name__ == "__main__":  main()
 
--- env/handyman/handyutil.py
+++ env/handyman/handyutil.py
@@ -3,13 +3,36 @@
 #util used by wrapper tool for automation and env config.
 #
 import argparse
-
 #from handyman_main import handyMantool
+#from handyman_main import cEvent
 import handyman_main
+#from handyman_main import *
 
-#from handyman_main import pSelf
-#import handyman_main as mm
-#from handyman_main import h1
+
+
+
+class cEvent(object):
+      def __init__(self, event_type=None,
+                   event_name=None,
+                   event_payload=None):
+       self._event_type = event_type
+       self._event_name = event_name
+       self._event_payload = event_payload
+       return
+      @property
+      def event_type(self):
+          return self._event_type
+      @event_type.setter
+      def set_event_type(self, value):
+          self._event_type = value
+      @property
+      def event_name(self):
+          return self._event_name
+      @event_name.setter
+      def event_name(self, value):
+          self._event_name = value
+      
+      
 
 def myprint(args):
   print args
@@ -21,16 +44,48 @@ def utilVer():
 	return
 
 
+def greetMe(string):	
+	
+	#e1 = cEvent("task", "greet", string)
+	hToolObj = None	
+	hToolObj = handyman_main.handyMantool.getToolInstance()	
+	#print hToolObj.getParamTable()
+	#hToolObj.appendParamTable([string, string])
+	#print hToolObj.getParamTable()
+	hToolObj.enqueue_new_event(cEvent("task", "greet", string))
+	return
+
 def utilParseParams(tmpObj=None):
 	
-	parser1 = argparse.ArgumentParser()
+	parser1 = argparse.ArgumentParser(description='The handyman is here.')
 
-	parser1.add_argument('greet', nargs='?', help='issue a greeting')
-	parser1.add_argument('opentunnels', nargs='?', help='open tunnels based on envconfig')
-	parser1.add_argument('listopentunnels', help='open tunnels based on envconfig', nargs='?')
 	
-	for entries in tmpObj.getParamTable():		
-		parser1.add_argument(entries[0], help=entries[1], nargs='?')
-
-	parser1.parse_args()                    
+	#parser1.add_argument('greet', nargs='?', help='issue a greeting')
+	#parser1.add_argument('opentunnels', nargs='?', help='open tunnels based on envconfig')
+	#parser1.add_argument('listopentunnels', help='open tunnels based on envconfig', nargs='?')
+	
+	#for entries in tmpObj.getParamTable():		
+	#	parser1.add_argument(entries[0], help=entries[1], nargs='?')
+	#parser1.add_argument("foobog", nargs='?', type=int)
+	parser1.add_argument("-t", "-task", help="specify the task name", \
+						required=False)
+	parser1.add_argument("-bookmark")
+	parser1.add_argument("-greet", type=greetMe)
+	args = parser1.parse_args()
+	
+	#print (args.task)	
 	return
+
+
+def utilApiCall():
+	z1 = handyman_main.handyMantool().getToolInstance()
+	z2 = handyman_main.handyMantool()._toolObj	
+	z2.holaecho('sing Bond')
+	z1.holaecho('sing Zoo')
+	return
+
+
+
+
+
+
