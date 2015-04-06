17b5c164f2c63e5c72fd163a6c9c01e18b1b4df8
Amit <amit213@users.noreply.github.com>
2015-04-05 19:23:39 -0700

    auto install of custom shell rc

--- env/prep-workspace.sh
+++ env/prep-workspace.sh
@@ -8,4 +8,8 @@ sudo apt-get install git -y
 git clone https://github.com/amit213/tools.git
 git clone https://amit213@bitbucket.org/amit213/mytools.git
 
+ln -s -f ./tools/env/myshellrc.rc ~/.myshellrc.rc
+echo "source ~/.myshellrc.rc" >> ~/.bashrc
+source ~/.bashrc
+
 
