e047cdcbaf88933896f2c137f40d182b16f0b823
TANK, AMIT <at883n@us.att.com>
2022-02-17 23:04:17 -0800

    update

--- env/vm-cloud-init-prep/vmware-vm-cloud-init-prep.sh
+++ env/vm-cloud-init-prep/vmware-vm-cloud-init-prep.sh
@@ -12,5 +12,4 @@ sudo ntpdate -s time.nist.gov;
 sudo timedatectl set-timezone America/Los_Angeles;
 # - sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 #curl -fsSL get.docker.com | sh;
-curl "https://api.telegram.org/bot5171666341:AAHxHeZLgSxf-G2psi8zku0qIsxbsTldHLg/sendMessage?chat_id=628822188&text='`hostname` is up at `hostname -I | awk '{print $1}'`'";
 
