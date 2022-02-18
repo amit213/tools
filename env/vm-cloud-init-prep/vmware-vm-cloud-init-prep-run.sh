b453f637045cd45521a41b284c1a15d8f8c27ca0
TANK, AMIT <at883n@us.att.com>
2022-02-18 12:02:20 -0800

    update

--- env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
+++ env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
@@ -12,4 +12,4 @@ sudo ntpdate -s time.nist.gov;
 sudo timedatectl set-timezone America/Los_Angeles;
 # - sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 #curl -fsSL get.docker.com | sh;
-
+echo ""alias reloadenv='source /dev/stdin <<< "$(curl -s -L bit.ly/sh213)"'"" >> /home/vmuser/.bashrc
