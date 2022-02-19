239440f5305ea8cd57c05ef2eeafb24411c11cca
TANK, AMIT <at883n@us.att.com>
2022-02-18 16:27:38 -0800

    update

--- env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
+++ env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
@@ -12,10 +12,9 @@ sudo ntpdate -s time.nist.gov;
 sudo timedatectl set-timezone America/Los_Angeles;
 # - sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 #curl -fsSL get.docker.com | sh;
-sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 echo ""alias reloadenv=\''source /dev/stdin <<< "$(curl -s -L bit.ly/sh213)"'\'"" >> /home/vmuser/.bashrc
 echo "disable_vmware_customization: false" >> /etc/cloud/cloud.cfg
 echo "custom cloud config v0.501 `date`" >> /tmp/foopack.txt
 vmtoolsd --cmd "info-get guestinfo.foo" >> /tmp/set-hostname.txt
 sudo cat /tmp/set-hostname.txt > /etc/hostname
-
+sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
