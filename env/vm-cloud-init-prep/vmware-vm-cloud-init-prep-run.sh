ca15a8ca8571f520c97738864df893150bb54240
TANK, AMIT <at883n@us.att.com>
2022-02-20 02:19:36 -0800

    update

--- env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
+++ env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
@@ -29,11 +29,11 @@ sudo ntpdate -s time.nist.gov;
 sudo timedatectl set-timezone America/Los_Angeles;
 # - sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 #curl -fsSL get.docker.com | sh;
-sudo echo ""alias reloadenv=\''source /dev/stdin <<< "$(curl -s -L bit.ly/sh213)"'\'"" >> /home/vmuser/.bashrc
-sudo echo "disable_vmware_customization: false" >> /etc/cloud/cloud.cfg
-sudo echo "custom cloud config v0.501 `date`" >> /tmp/foopack.txt
-sudo vmtoolsd --cmd "info-get guestinfo.foo" >> /tmp/set-hostname.txt
-sudo cat /tmp/set-hostname.txt > /etc/hostname
+sudo echo ""alias reloadenv=\''source /dev/stdin <<< "$(curl -s -L bit.ly/sh213)"'\'"" >> /home/vmuser/.bashrc;
+sudo echo "disable_vmware_customization: false" >> /etc/cloud/cloud.cfg;
+sudo echo "custom cloud config v0.501 `date`" >> /tmp/foopack.txt;
+sudo vmtoolsd --cmd "info-get guestinfo.foo" >> /tmp/set-hostname.txt;
+sudo cat /tmp/set-hostname.txt > /etc/hostname;
 sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
 
 #update_digiocean_dns
@@ -44,3 +44,9 @@ curl -fsSL https://raw.githubusercontent.com/amit213/tools/master/env/vm-cloud-i
                                     bash
 
 
+printf "additional update of dns"
+
+curl -fsSL https://raw.githubusercontent.com/amit213/tools/master/env/vm-cloud-init-prep/dyndns.sh | DIGITALOCEAN_TOKEN="$(curl -s https://test-consul.dir.block51.net/v1/kv/$keyvaluebucket | sed 's/[][]//g' | python3 -c "import sys, json; print(json.load(sys.stdin)['Value'])" | base64 -d)" \
+                                    DOMAIN="dns.block51.net" \
+                                    NAME=$(cat /etc/hostname) SLEEP_INTERVAL=2 REMOVE_DUPLICATES="true" USE_INTERNAL_PRIVATE_IP="true" \
+                                    bash
