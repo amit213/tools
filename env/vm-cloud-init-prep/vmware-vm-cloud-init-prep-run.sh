ffe43f16dc8abfa25b30bbcc78a5d4e8810dd462
TANK, AMIT <at883n@us.att.com>
2022-02-20 00:30:35 -0800

    update

--- env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
+++ env/vm-cloud-init-prep/vmware-vm-cloud-init-prep-run.sh
@@ -8,7 +8,7 @@
 
 sudo apt-get update -y;
 #sudo apt-get install sl fail2ban iputils-ping curl ntpdate python3-pip vim libffi-dev libssl-dev open-vm-tools --fix-missing -y;
-sudo apt-get install docker-compose -y;
+sudo apt-get install docker-compose jq -y;
 sudo ntpdate -s time.nist.gov;
 sudo timedatectl set-timezone America/Los_Angeles;
 # - sudo sed -i "2i`hostname -I | awk '{print $1}'` `cat /etc/hostname`" /etc/hosts
