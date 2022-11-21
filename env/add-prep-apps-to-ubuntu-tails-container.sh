a9e1c90dc11f5621085cb6b656edc0f4f721b0ff
TANK, AMIT <at883n@us.att.com>
2022-11-20 19:52:25 -0800

    update

--- env/add-prep-apps-to-ubuntu-tails-container.sh
+++ env/add-prep-apps-to-ubuntu-tails-container.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -eu
 
 
+function install_telegram_utils() {
+	mkdir -p /home/vmuser/scratch/tools-utils/telegram.sh;
+	git clone https://github.com/fabianonline/telegram.sh.git /home/vmuser/scratch/tools-utils/telegram.sh;
+	sudo ln -s -f /home/vmuser/scratch/tools-utils/telegram.sh/telegram /usr/local/bin/telegram
+	sudo ln -s -f /home/vmuser/scratch/tools-utils/telegram.sh/test.sh /usr/local/bin/test.sh
+}
+
+
 function add_general_apps() {
-	touch /tmp/rambozebra321.txt;
-	echo "howdy zebra 321" >> /tmp/rambozebra321.txt;
+	#touch /tmp/rambozebra321.txt;
+	#echo "howdy zebra 321" >> /tmp/rambozebra321.txt;
+	install_telegram_utils
 }
 
 
