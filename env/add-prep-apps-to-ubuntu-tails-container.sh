afc3115d8b4137391de2fb813038f0058acc33ad
TANK, AMIT <at883n@us.att.com>
2022-11-20 20:42:08 -0800

    update

--- env/add-prep-apps-to-ubuntu-tails-container.sh
+++ env/add-prep-apps-to-ubuntu-tails-container.sh
@@ -12,7 +12,9 @@ function install_system_apps() {
 	sudo apt install -y ncdu; 
 
 	sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-	sudo dpkg -i google-chrome-stable_current_amd64.deb;
+	sudo apt install ./google-chrome-stable_current_amd64.deb -y;
+    sudo apt -f install -y;
+
 }
 
 
